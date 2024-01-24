ffb83905c8acfc277894b59e784409988dc35207
iomgaa <iomgaaycz@gmail.com>
2024-01-24 02:43:18 +0000

    睡姿数据上传

--- LMDB/controller/splite.py
+++ LMDB/controller/splite.py
@@ -9,50 +9,49 @@ import shutil
 os.environ['WANDB_BASE_URL'] = "http://192.168.1.121:1123"
 os.environ['WANDB_API_KEY'] = "local-00f57935806148c0ce6b0c5623b2b826ad2ee681"
 
-def split_databases(db_path, db_names):
+def split_databases(db_path, train_names=["yuchengzhang"], val_names=["baishuhang"]):
     
     val_data = []
     train_data = []
     data_dict = {}
     lmdb_manager = LMDBManager(db_path=db_path)
-    # lmdb_manager.second_db = lmdb_manager.env.open_db("train".encode('utf-8'))
-    # lmdb_manager.hypter_db = lmdb_manager.env.open_db("val".encode('utf-8'))
-    # lmdb_manager.clear_databases()
-    for name in db_names:
+    lmdb_manager.second_db = lmdb_manager.env.open_db("train".encode('utf-8'))
+    lmdb_manager.hypter_db = lmdb_manager.env.open_db("val".encode('utf-8'))
+    keys = lmdb_manager.get_keys()
+    if len(keys) == 0:
+        print("The database has already been splited!")
+    lmdb_manager.clear_databases()
+    for name in train_names:
         lmdb_manager.second_db = lmdb_manager.env.open_db(name.encode('utf-8'))
         keys = lmdb_manager.get_keys()
         for key in keys:
             value_json = lmdb_manager.read(key)
             value = json.loads(value_json)
             if value["action"] not in ["正卧（一级）", "正卧（二级）", "俯卧（一级）", "俯卧（二级）", "左侧卧（一级）", "左侧卧（二级）", "右侧卧（一级）", "右侧卧（二级）"]:
-                val_data.append(key)
+                continue
+            else:
+                train_data.append(key)
                 data_dict[key] = name
-                # continue
+
+    for name in val_names:
+        lmdb_manager.second_db = lmdb_manager.env.open_db(name.encode('utf-8'))
+        keys = lmdb_manager.get_keys()
+        for key in keys:
+            value_json = lmdb_manager.read(key)
+            value = json.loads(value_json)
+            if value["action"] not in ["正卧（一级）", "正卧（二级）", "俯卧（一级）", "俯卧（二级）", "左侧卧（一级）", "左侧卧（二级）", "右侧卧（一级）", "右侧卧（二级）"]:
+                continue
             else:
                 val_data.append(key)
                 data_dict[key] = name
-
-    # num_keys = len(val_data) + len(train_data)
-    # print("Total number of elements: ", num_keys)
     print("Number of elements in val: ", len(val_data))
     print("Number of elements in train: ", len(train_data))
-    # Calculate the number of elements to be sent to "val" database
-    # val_count = num_keys - int(num_keys * 0.3)
     
-    
-    # Move remaining elements to "val" database if count is not reached
-    # if len(val_data) < val_count:
-    #     remaining_count = val_count - len(val_data)
-    #     for key in train_data:
-    #         val_data.append(key)
-    #         train_data.remove(key)
-    #         remaining_count -= 1
-    #         if remaining_count == 0:
-    #             break
     
     # Write train_data to "train" database
     lmdb_manager.hypter_db = lmdb_manager.env.open_db("train".encode('utf-8'))
     db_name = "yuchengzhang"
+    lmdb_manager.second_db = lmdb_manager.env.open_db(db_name.encode('utf-8'))
     for key in train_data:
         if db_name != data_dict[key]:
             lmdb_manager.second_db = lmdb_manager.env.open_db(data_dict[key].encode('utf-8'))
@@ -63,6 +62,7 @@ def split_databases(db_path, db_names):
     # Write val_data to "val" database
     lmdb_manager.hypter_db = lmdb_manager.env.open_db("val".encode('utf-8'))
     db_name = "yuchengzhang"
+    lmdb_manager.second_db = lmdb_manager.env.open_db(db_name.encode('utf-8'))
     for key in val_data:
         if db_name != data_dict[key]:
             lmdb_manager.second_db = lmdb_manager.env.open_db(data_dict[key].encode('utf-8'))
@@ -75,13 +75,13 @@ def split_databases(db_path, db_names):
 
 
 
-# split_databases(db_path="./LMDB/database", db_names=["baishuhang"])
+split_databases(db_path="./LMDB/database", train_names=["yuchengzhang","shunjian","fanghaolun"], val_names=["baishuhang"])
 run = wandb.init(project="RestNightAI", job_type="load-data")
 raw_data = wandb.Artifact(
-            "Onbed_data", type="dataset",
-            description="在床检测数据集，包含余承璋与白书航两个人的数据。余承璋的正例作为训练集，白书航的正例和负例作为验证集。",
+            "Sleep_Pose_data", type="dataset",
+            description="睡姿检测数据集，包含余承璋与白书航两个人的数据。余承璋的数据作为训练集，白书航的数据作为验证集。",
             metadata={"subject": ["yuchengzhang", "baishuhang"],
-                      "type": "Onbed"})
+                      "type": "Sleep Pose"})
 raw_data.add_file("./LMDB/database.zip")
 run.log_artifact(raw_data)
 print("Done")
